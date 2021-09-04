f49862da6fb4df05bdd1bee1f24d62393f35557f
thuonghaivn <87941075+thuonghaivn@users.noreply.github.com>
2021-09-04 14:14:17 +0700

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,8 @@
+@echo off
+set region=eu
+nslookup 0.tcp.%region%.ngrok.io > IP.txt
+findstr /n "." IP.txt | findstr "5:Address:"
+for /f "tokens=3 delims=: " %%i  in ('findstr /n "." IP.txt ^| findstr "5:Address:"') do set ip=%%i
+curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url > tunnel.txt
+powershell -Command "(gc tunnel.txt) -replace 'tcp://0.tcp.%region%.ngrok.io', '%ip%' | Out-File -encoding ASCII IP.txt"
+type IP.txt
